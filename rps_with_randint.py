3a3959f252e3fb8994249f14d007858e1aa34310
RaviC19 <ravic1919@gmail.com>
2021-04-06 00:54:08 +0100

    Updated randint rps game to use a while loop that runs until either the player or the computer reaches 5 wins

--- rps_with_randint.py
+++ rps_with_randint.py
@@ -1,38 +1,58 @@
 from random import randint
 
-player = input("What is your choice? ").lower()
-computer = randint(0, 2)
+computer_wins = 0
+player_wins = 0
+winning_score = 5
 
-if computer == 0:
-    computer = "Rock"
-elif computer == 1:
-    computer = "Paper"
-else:
-    computer = "Scissors"
+while computer_wins < winning_score and player_wins < winning_score:
+    player = input("What is your choice? ").lower()
+    computer = randint(0, 2)
 
-print(f"The computer chose {computer}")
+    if computer == 0:
+        computer = "Rock"
+    elif computer == 1:
+        computer = "Paper"
+    else:
+        computer = "Scissors"
 
-if player.lower() == computer.lower():
-    print("You and the computer chose the same option, this game is a draw")
-
-elif player == "rock":
-    if computer == "Scissors":
-        print("You chose Rock and the computer chose Scissors. Your Rock Wins!")
-    elif computer == "Paper":
-        print("You chose Rock and the computer chose Paper. The Computer's Paper Wins!")
-
-elif player == "paper":
-    if computer == "Rock":
-        print("You chose Paper and the computer chose Rock. Your Paper Wins!")
-    elif computer == "Scissors":
+    if player.lower() == computer.lower():
         print(
-            "You chose Paper and the computer chose Scissors. The Computer's Scissors Wins!")
+            f"You and the computer chose the same option, this game is a draw. The score is you have {player_wins} whilst the computer has {computer_wins}")
 
-elif player == "scissors":
-    if computer == "Rock":
-        print("You chose Scissors and the computer chose Rock. The Computer's Rock Wins!")
-    elif computer == "Paper":
-        print("You chose Scissors and the computer chose Paper. Your Scissors Wins!")
+    elif player == "rock":
+        if computer == "Scissors":
+            player_wins += 1
+            print(
+                f"You chose Rock and the computer chose Scissors. Your Rock Wins! The score is you have {player_wins} whilst the computer has {computer_wins}")
+        elif computer == "Paper":
+            computer_wins += 1
+            print(
+                f"You chose Rock and the computer chose Paper. The Computer's Paper Wins! The score is you have {player_wins} whilst the computer has {computer_wins}")
 
-else:
-    print("You entered something that wasn't Rock, Paper or Scissors")
+    elif player == "paper":
+        if computer == "Rock":
+            player_wins += 1
+            print(
+                f"You chose Paper and the computer chose Rock. Your Paper Wins! The score is you have {player_wins} whilst the computer has {computer_wins}")
+        elif computer == "Scissors":
+            computer_wins += 1
+            print(
+                f"You chose Paper and the computer chose Scissors. The Computer's Scissors Wins! The score is you have {player_wins} whilst the computer has {computer_wins}")
+
+    elif player == "scissors":
+        if computer == "Rock":
+            computer_wins += 1
+            print(
+                f"You chose Scissors and the computer chose Rock. The Computer's Rock Wins! The score is you have {player_wins} whilst the computer has {computer_wins}")
+        elif computer == "Paper":
+            player_wins += 1
+            print(
+                f"You chose Scissors and the computer chose Paper. Your Scissors Wins! The score is you have {player_wins} whilst the computer has {computer_wins}")
+
+    else:
+        print("You entered something that wasn't Rock, Paper or Scissors")
+
+if computer_wins > player_wins:
+    print("The computer reached 5 before you could so unfortunately you lost!")
+elif player_wins > computer_wins:
+    print("Congratulations! You got to 5 wins before the computer could and you won!")
--- rps_with_random_choice.py
+++ rps_with_random_choice.py
@@ -1,34 +1,36 @@
 from random import choice
+for i in range(3):
+    player = input("What is your choice? ").lower()
+    options = ["Rock", "Paper", "Scissors"]
 
-player = input("What is your choice? ").lower()
-options = ["Rock", "Paper", "Scissors"]
+    computer = choice(options)
 
-computer = choice(options)
+    print(player)
+    print(f"The Computer chose {computer}")
 
-print(player)
-print(f"The Computer chose {computer}")
+    if player.lower() == computer.lower():
+        print("You and the computer chose the same option, this game is a draw")
 
-if player.lower() == computer.lower():
-    print("You and the computer chose the same option, this game is a draw")
+    elif player == "rock":
+        if computer == "Scissors":
+            print("You chose Rock and the computer chose Scissors. Your Rock Wins!")
+        elif computer == "Paper":
+            print(
+                "You chose Rock and the computer chose Paper. The Computer's Paper Wins!")
 
-elif player == "rock":
-    if computer == "Scissors":
-        print("You chose Rock and the computer chose Scissors. Your Rock Wins!")
-    elif computer == "Paper":
-        print("You chose Rock and the computer chose Paper. The Computer's Paper Wins!")
+    elif player == "paper":
+        if computer == "Rock":
+            print("You chose Paper and the computer chose Rock. Your Paper Wins!")
+        elif computer == "Scissors":
+            print(
+                "You chose Paper and the computer chose Scissors. The Computer's Scissors Wins!")
 
-elif player == "paper":
-    if computer == "Rock":
-        print("You chose Paper and the computer chose Rock. Your Paper Wins!")
-    elif computer == "Scissors":
-        print(
-            "You chose Paper and the computer chose Scissors. The Computer's Scissors Wins!")
+    elif player == "scissors":
+        if computer == "Rock":
+            print(
+                "You chose Scissors and the computer chose Rock. The Computer's Rock Wins!")
+        elif computer == "Paper":
+            print("You chose Scissors and the computer chose Paper. Your Scissors Wins!")
 
-elif player == "scissors":
-    if computer == "Rock":
-        print("You chose Scissors and the computer chose Rock. The Computer's Rock Wins!")
-    elif computer == "Paper":
-        print("You chose Scissors and the computer chose Paper. Your Scissors Wins!")
-
-else:
-    print("You entered something that wasn't Rock, Paper or Scissors")
+    else:
+        print("You entered something that wasn't Rock, Paper or Scissors")
